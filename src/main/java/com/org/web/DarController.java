b212f5c7fe9e9971815d23454bf2fabab6ad03ff
sumit <sumit.esri@gmail.com>
2018-08-05 18:11:41 +0530

    Implemented DSR and DAR

---
+++ src/main/java/com/org/web/DarController.java
@@ -0,0 +1,46 @@
+package com.org.web;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.org.service.blobstore.FileStorageService;
+
+@RequestMapping("/books/dar")
+@Controller
+public class DarController {
+	
+	@Autowired
+	private FileStorageService fileStorageService;
+
+    @RequestMapping(method = RequestMethod.POST, value = "{id}")
+    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    @RequestMapping
+    public String index() {
+        return "books/dar/index";
+    }
+    
+    @RequestMapping("/download")
+	public String download(@RequestParam(value = "file", required = false) String file, HttpServletResponse response, Model model) {
+		try {
+            response.setHeader("Content-Disposition", "attachment;filename=\"" + file + "\"");
+			System.out.println("URL--------------"+file);
+			fileStorageService.doGet(response, file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
--- src/main/resources/sql/UPDATE_V1.sql
+++ src/main/resources/sql/UPDATE_V1.sql
@@ -4,6 +4,7 @@ ALTER TABLE `measurement_sheet` ADD `TEMPLATE_VERSION` INT NOT NULL AFTER `log_u
 -- Table structure for table `managed_document`
 --
 
+
 CREATE TABLE `managed_document` (
   `id` bigint(20) NOT NULL,
   `description` varchar(500) DEFAULT NULL,
@@ -11,7 +12,8 @@ CREATE TABLE `managed_document` (
   `filename` varchar(50) DEFAULT NULL,
   `url` varchar(200) NOT NULL,
   `version` int(11) DEFAULT NULL,
-  `aggreement` bigint(20) DEFAULT NULL
+  `aggreement` bigint(20) DEFAULT NULL,
+  `log_user` bigint(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -23,7 +25,8 @@ CREATE TABLE `managed_document` (
 --
 ALTER TABLE `managed_document`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `FK5715BB652DCF72` (`aggreement`);
+  ADD KEY `FK5715BB652DCF72` (`aggreement`),
+  ADD KEY `FK5715BBF6EC5FA2` (`log_user`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -33,7 +36,7 @@ ALTER TABLE `managed_document`
 -- AUTO_INCREMENT for table `managed_document`
 --
 ALTER TABLE `managed_document`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 --
 -- Constraints for dumped tables
 --
@@ -42,5 +45,6 @@ ALTER TABLE `managed_document`
 -- Constraints for table `managed_document`
 --
 ALTER TABLE `managed_document`
-  ADD CONSTRAINT `FK5715BB652DCF72` FOREIGN KEY (`aggreement`) REFERENCES `aggreement` (`id`);
+  ADD CONSTRAINT `FK5715BB652DCF72` FOREIGN KEY (`aggreement`) REFERENCES `aggreement` (`id`),
+  ADD CONSTRAINT `FK5715BBF6EC5FA2` FOREIGN KEY (`log_user`) REFERENCES `log_user` (`id`);
 COMMIT;
